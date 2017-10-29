6885ad7deaa73bcc2ec1bc04be85eefb284442a9
root <root@DESKTOP-2CUETC3>
2017-10-29 11:45:37 +0100

    Init

---
+++ src/main/java/de/vogma/cpu/assembler/model/InstructionSet.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package de.vogma.cpu.assembler.model;
+
+/**
+ *
+ * @author root
+ */
+public enum InstructionSet {
+    NOP, MOV, IN, STO, LOD, PUSH, POP, PRN, CALL, RTS, ADD, SUB, INC, DEC, COMP, SHIFTL, SHIFTR, ROTL, ROTR, AND, OR, NOR, NAND, XOR, XNOR
+}
